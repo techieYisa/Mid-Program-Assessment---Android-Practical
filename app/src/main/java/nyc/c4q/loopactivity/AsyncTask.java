021aeeb63dbddd81fbe5d8c0771b0c84780ddc2b
techieYisa <yisandymarte@ac.c4q.nyc>
2017-12-06 19:31:36 -0500

    added part01

---
+++ app/src/main/java/nyc/c4q/loopactivity/AsyncTask.java
@@ -0,0 +1,22 @@
+package nyc.c4q.loopactivity;
+
+/**
+ * Created by c4q on 12/6/17.
+ */
+
+public class AsyncTask extends android.os.AsyncTask<Integer, Integer, Integer> {
+    @Override
+    protected void onPreExecute() {
+    }
+
+    @Override
+    protected Integer doInBackground(Integer[] value) {
+}
+    @Override
+    protected void onProgressUpdate(Integer... progress) {
+    }
+
+    @Override
+    protected void onPostExecute(Integer result){
+    }
+}
